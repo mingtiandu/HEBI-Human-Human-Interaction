e1e5d5329f8295c7d7f130e417623742713f66ea
mingtiandu <mingtiandu@outlook.com>
2025-03-03 16:07:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Hebi Robotics setup for robot-mediated Dyadic (Human-human) Interactions
+## Dependencies
+Install python minimum required python 3.6 from:
+https://www.python.org/downloads/
+
+Install cmake minimum required cmake 3.12 from:
+https://cmake.org/download/
+
+### For windows
+Install visual studio from:
+https://visualstudio.microsoft.com/downloads/
+
+Install _desktop development with C++_ from Visual Studio Installer
+
+## Startup
+Start the terminal from root directory
+```
+mkdir build
+cd build
+cmake -G"Visual Studio 16 2019" -Ax64 ../projects/cmake/
+```
+Then open the __build/hebi_human_human.sln__ from visual studio 
---
+++ projects/cmake/DownloadHebiCpp.cmake
@@ -0,0 +1,32 @@
+# Used to download the C++ API - this should not be used directly.
+cmake_minimum_required(VERSION 3.0)
+
+set(HEBI_CPP_VERSION "3.11.1")
+set(HEBI_CPP_FILE_NAME "hebi-cpp-${HEBI_CPP_VERSION}.tar.gz")
+set(HEBI_CPP_LIB_SHA256 "26454863b4f4d9dd7902c8d7530fc70f766353b0aed97c33781f897874c3d7dd")
+set(HEBI_CPP_URL "https://files.hebi.us/download/cpp/${HEBI_CPP_FILE_NAME}")
+
+# If the CMakeLists.txt is not found, then redownload the C++ API 
+if(NOT EXISTS ${HEBI_DIR}/CMakeLists.txt)
+
+message(STATUS "Downloading C++ API version ${HEBI_CPP_VERSION} from ${HEBI_CPP_URL}")
+get_filename_component(HEBI_CPP_FILE_FULL_PATH "${CMAKE_BINARY_DIR}/${HEBI_CPP_FILE_NAME}" REALPATH)
+file(DOWNLOAD "${HEBI_CPP_URL}" "${HEBI_CPP_FILE_FULL_PATH}"
+  EXPECTED_HASH SHA256=${HEBI_CPP_LIB_SHA256}
+  SHOW_PROGRESS)
+
+# Try and find a python install
+find_program(PYTHON_EXECUTABLE python)
+
+if(EXISTS "${PYTHON_EXECUTABLE}")
+  execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
+  "import tarfile; tar = tarfile.open(r'${HEBI_CPP_FILE_FULL_PATH}'); tar.extractall(path=r'${ROOT_DIR}'); tar.close()")
+# Fall back on `tar` for MacOS and Linux
+elseif(NOT EXISTS "${PYTHON_EXECUTABLE}" AND NOT WIN32)
+  execute_process(COMMAND "tar" "-xvf" "${HEBI_CPP_FILE_FULL_PATH}"
+    WORKING_DIRECTORY ${ROOT_DIR})
+elseif(WIN32)
+  message(FATAL_ERROR "Cannot extract ${HEBI_CPP_FILE_FULL_PATH} because python was not found in the executable path. Extract this file manually and re-run CMake.")
+endif()
+
+endif()
